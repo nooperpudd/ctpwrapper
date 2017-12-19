dcffda7d966a40bff29c7204e6a6d310f100c14a
wang.weichen <weichen.wang@jsxforg.com>
2017-12-19 23:17:28 +0800

    fix

--- ctpwrapper/MdApi.pyx
+++ ctpwrapper/MdApi.pyx
@@ -5,10 +5,10 @@
 from Cpython.mem cimport PyMem_Malloc, PyMem_Realloc, PyMem_Free
 from libc.stdlib cimport atoi
 
-from cMdAPI cimport
+from cMdAPI cimport CMdSpi
 
 cdef class CThostFtdcMdSpi:
-    cdef CThostFtdcMdApi*  _this_Ftdc_Md_Spi
+    cdef CMdSpi*  _this_Ftdc_Md_Spi
 
     def __cinit__(self):
         pass
@@ -16,22 +16,22 @@ cdef class CThostFtdcMdSpi:
     def __dealloc__(self):
         pass
 
-    cpdef on_front_connected(self, ) except? -1:
-        pass
+    # cpdef on_front_connected(self, ) except? -1:
+    #     pass
+    #
+    # cpdef on_front_disconnected(self, int reason) except? -1:
+    #     pass
+    #
+    # cpdef on_heart_beat_warning(self, int time_lapse) except? -1:
+    #     pass
+    #
+    # cpdef on_user_login(self,):
+    #     pass
 
-    cpdef on_front_disconnected(self, int reason) except? -1:
-        pass
-
-    cpdef on_heart_beat_warning(self, int time_lapse) except? -1:
-        pass
-
-    cpdef on_user_login(self,):
-        pass
-
-cdef class CThostFtdcMdApi:
-    def __cinit__(self):
-        pass
-
-    def __dealloc__(self):
-        pass
+# cdef class CThostFtdcMdApi:
+#     def __cinit__(self):
+#         pass
+#
+#     def __dealloc__(self):
+#         pass
 
