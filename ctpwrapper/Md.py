c646222df09f9ba6a56e00506ca0fba15b540976
wangweichen <weichen.wang@jsxfedu.com>
2017-12-22 21:15:19 +0800

    add md

---
+++ ctpwrapper/Md.py
@@ -0,0 +1,106 @@
+# encoding:utf-8
+
+# from .MdApi import
+
+
+class MdClass(object):
+    def __init__(self):
+        pass
+
+    def GetApiVersion(self):
+        """
+        获取API的版本信息
+        @retrun 获取到的版本号
+        :return: 
+        """
+        pass
+
+    def ReqUserLogin(self, pReqUserLoginField, nRequestID):
+        """
+        用户登录请求
+        :return:
+        """
+        pass
+
+    def ReqUserLogout(self, pUserLogout, nRequestID):
+        """
+         登出请求
+        :return:
+        """
+        pass
+
+    def GetTradingDay(self):
+        """
+        获取当前交易日
+        @retrun 获取到的交易日
+        @remark 只有登录成功后,才能得到正确的交易日
+        :return:
+        """
+        pass
+
+    def RegisterFront(self, pszFrontAddress):
+        """
+        注册前置机网络地址
+        @param pszFrontAddress：前置机网络地址。
+        @remark 网络地址的格式为：“protocol://ipaddress:port”，如：”tcp://127.0.0.1:17001”。
+        @remark “tcp”代表传输协议，“127.0.0.1”代表服务器地址。”17001”代表服务器端口号。
+        :return:
+        """
+        pass
+
+    def RegisterNameServer(self, pszNsAddress):
+        """
+        注册名字服务器网络地址
+        @param pszNsAddress：名字服务器网络地址。
+        @remark 网络地址的格式为：“protocol://ipaddress:port”，如：”tcp://127.0.0.1:12001”。
+        @remark “tcp”代表传输协议，“127.0.0.1”代表服务器地址。”12001”代表服务器端口号。
+        @remark RegisterNameServer优先于RegisterFront
+        :return:
+        """
+        pass
+
+    def RegisterFensUserInfo(self, pFensUserInfo):
+        """
+        注册名字服务器用户信息
+        @param pFensUserInfo：用户信息。
+        :return:
+        """
+        pass
+
+    def SubscribeMarketData(self, pInstrumentID):
+        """
+         订阅行情。
+        @param ppInstrumentID 合约ID
+        @param nCount 要订阅/退订行情的合约个数
+
+        :return:
+        """
+        pass
+
+    def UnSubscribeMarketData(self, pInstrumentID):
+        """
+        退订行情。
+        @param ppInstrumentID 合约ID
+        @param nCount 要订阅/退订行情的合约个数
+        :return:
+        """
+        pass
+
+    def SubscribeForQuoteRsp(self, pInstrumentID):
+        """
+        订阅询价。
+        :param pInstrumentID: 合约ID list
+
+        :return:
+        """
+        pass
+
+    def UnSubscribeForQuoteRsp(self, pInstrumentID):
+        """
+        退订询价。
+        :param pInstrumentID: 合约ID
+        :return:
+        """
+        pass
+
+
