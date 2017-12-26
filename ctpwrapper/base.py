ef733c915668c282b0cc8ad75d47ed740807d80d
wangweichen <weichen.wang@jsxfedu.com>
2017-12-26 18:52:42 +0800

    fix

--- ctpwrapper/base.py
+++ ctpwrapper/base.py
@@ -1,23 +1,67 @@
 # encoding:utf-8
 
+import ctypes
 
-class Base(object):
+import json
 
-    def from_dict(self, obj):
+
+class Base(ctypes.Structure):
+
+    def _to_bytes(self, value):
         """
         :return:
         """
-        pass
+        if isinstance(value, bytes):
+            return value
+        else:
+            return bytes(value, encoding="utf-8")
 
-    def dict(self):
+    @classmethod
+    def from_dict(cls, obj):
         """
         :return:
         """
-        return {key: value for key, value in self.__dict__.items()}
+        return cls(**obj)
+
+    def to_dict(self):
+        """
+        :return:
+        """
+        results = {}
+        for key, value in self._fields_:
+            _value = getattr(self, key)
+            if isinstance(_value, bytes):
+                results[key] = _value.decode()
+            else:
+                results[key] = _value
+        return results
 
     def __repr__(self):
         """
         :return:
         """
-        items = ["%s:%s" % (item, value) for item, value in self.__dict__.items()]
+        items = ["%s:%s" % (item, getattr(self, item)) for item, value in self._fields_]
         return "%s<%s>" % (self.__class__.__name__, ",".join(items))
+
+
+class D(Base):
+    _fields_ = [("hello", ctypes.c_double),
+                ("nihao", ctypes.c_char_p)]
+
+    def __init__(self, hello="", nihao=""):
+        super(D, self).__init__()
+        self.hello = float(hello)
+        # a=
+        self.nihao = self._to_bytes(nihao)
+
+
+gg = {"hello": "1"}
+dd = D.from_dict(gg)
+print(dd)
+print(dd.to_dict())
+
+d = D(hello=1, nihao="a")
+print(d.to_dict())
+e = json.dumps(d.to_dict())
+print(d)
+print(e)
