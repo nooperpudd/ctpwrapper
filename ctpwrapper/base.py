efa050e3c97481e06f5415296c04d10085e86d8e
wang.weichen <weichen.wang@jsxforg.com>
2017-12-30 23:54:29 +0800

    add

--- ctpwrapper/base.py
+++ ctpwrapper/base.py
@@ -1,8 +1,5 @@
 # encoding:utf-8
-
 import ctypes
-
-import json
 
 
 class Base(ctypes.Structure):
@@ -42,26 +39,3 @@ class Base(ctypes.Structure):
         """
         items = ["%s:%s" % (item, getattr(self, item)) for item, value in self._fields_]
         return "%s<%s>" % (self.__class__.__name__, ",".join(items))
-
-
-class D(Base):
-    _fields_ = [("hello", ctypes.c_double),
-                ("nihao", ctypes.c_char_p)]
-
-    def __init__(self, hello="", nihao=""):
-        super(D, self).__init__()
-        self.hello = float(hello)
-        # a=
-        self.nihao = self._to_bytes(nihao)
-
-
-gg = {"hello": "1"}
-dd = D.from_dict(gg)
-print(dd)
-print(dd.to_dict())
-
-d = D(hello=1, nihao="a")
-print(d.to_dict())
-e = json.dumps(d.to_dict())
-print(d)
-print(e)
