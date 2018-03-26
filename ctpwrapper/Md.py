dc7d0f50cf7b57f49269d320cd7a8b4bcf9d43ad
wang.weichen <weichen.wang@jsxforg.com>
2018-03-26 22:19:31 +0800

    add

--- ctpwrapper/Md.py
+++ ctpwrapper/Md.py
@@ -3,79 +3,85 @@
 from ctpwrapper.MdApi import MdApiWrapper
 
 
-class MdSpiPy(object):
-
-    # 当客户端与交易后台建立起通信连接时（还未登录前），该方法被调用。
-    def OnFrontConnected(self):
-        pass
-
-    # 当客户端与交易后台通信连接断开时，该方法被调用。当发生这个情况后，API会自动重新连接，客户端可不做处理。
-    # @param nReason 错误原因
-    #         0x1001 网络读失败
-    #         0x1002 网络写失败
-    #         0x2001 接收心跳超时
-    #         0x2002 发送心跳失败
-    #         0x2003 收到错误报文
-
-    # 4097 0x1001 网络读失败
-    # 4098 0x1002 网络写失败
-    # 8193 0x2001 读心跳超时
-    # 8194 0x2002 发送心跳超时
-    # 8195 0x2003 收到不能识别的错误消息
-    # 客户端与服务端的连接断开有两种情况：
-    #  网络原因导致连接断开
-    #  服务端主动断开连接
-    # 服务器主动断开连接有两种可能：
-    #  客户端长时间没有从服务端接收报文，时间超时
-    #  客户端建立的连接数超过限制
-    def OnFrontDisconnected(self, nReason):
-        pass
-
-    # 心跳超时警告。当长时间未收到报文时，该方法被调用。
-    # @param nTimeLapse 距离上次接收报文的时间
-    def OnHeartBeatWarning(self, nTimeLapse):
-        pass
-
-    # 登录请求响应
-    def OnRspUserLogin(self, pRspUserLogin, pRspInfo, nRequestID, bIsLast):
-        pass
-
-    # 登出请求响应
-    def OnRspUserLogout(self, pUserLogout, pRspInfo, nRequestID, bIsLast):
-        pass
-
-    # 错误应答
-    def OnRspError(self, pRspInfo, nRequestID, bIsLast):
-        pass
-
-    # 订阅行情应答
-    def OnRspSubMarketData(self, pSpecificInstrument, pRspInfo, nRequestID, bIsLast):
-        pass
-
-    # 取消订阅行情应答
-    def OnRspUnSubMarketData(self, pSpecificInstrument, pRspInfo, nRequestID, bIsLast):
-        pass
-
-    # 订阅询价应答
-    def OnRspSubForQuoteRsp(self, pSpecificInstrument, pRspInfo, nRequestID, bIsLast):
-        pass
-
-    # 取消订阅询价应答
-    def OnRspUnSubForQuoteRsp(self, pSpecificInstrument, pRspInfo, nRequestID, bIsLast):
-        pass
-
-    # 深度行情通知
-    def OnRtnDepthMarketData(self, pDepthMarketData):
-        pass
-
-    # 询价通知
-    def OnRtnForQuoteRsp(self, pForQuoteRsp):
-        pass
+# class MdSpiPy(object):
+#
+#     # 当客户端与交易后台建立起通信连接时（还未登录前），该方法被调用。
+#     def OnFrontConnected(self):
+#         pass
+#
+#     # 当客户端与交易后台通信连接断开时，该方法被调用。当发生这个情况后，API会自动重新连接，客户端可不做处理。
+#     # @param nReason 错误原因
+#     #         0x1001 网络读失败
+#     #         0x1002 网络写失败
+#     #         0x2001 接收心跳超时
+#     #         0x2002 发送心跳失败
+#     #         0x2003 收到错误报文
+#
+#     # 4097 0x1001 网络读失败
+#     # 4098 0x1002 网络写失败
+#     # 8193 0x2001 读心跳超时
+#     # 8194 0x2002 发送心跳超时
+#     # 8195 0x2003 收到不能识别的错误消息
+#     # 客户端与服务端的连接断开有两种情况：
+#     #  网络原因导致连接断开
+#     #  服务端主动断开连接
+#     # 服务器主动断开连接有两种可能：
+#     #  客户端长时间没有从服务端接收报文，时间超时
+#     #  客户端建立的连接数超过限制
+#     def OnFrontDisconnected(self, nReason):
+#         pass
+#
+#     # 心跳超时警告。当长时间未收到报文时，该方法被调用。
+#     # @param nTimeLapse 距离上次接收报文的时间
+#     def OnHeartBeatWarning(self, nTimeLapse):
+#         pass
+#
+#     # 登录请求响应
+#     def OnRspUserLogin(self, pRspUserLogin, pRspInfo, nRequestID, bIsLast):
+#         pass
+#
+#     # 登出请求响应
+#     def OnRspUserLogout(self, pUserLogout, pRspInfo, nRequestID, bIsLast):
+#         pass
+#
+#     # 错误应答
+#     def OnRspError(self, pRspInfo, nRequestID, bIsLast):
+#         pass
+#
+#     # 订阅行情应答
+#     def OnRspSubMarketData(self, pSpecificInstrument, pRspInfo, nRequestID, bIsLast):
+#         pass
+#
+#     # 取消订阅行情应答
+#     def OnRspUnSubMarketData(self, pSpecificInstrument, pRspInfo, nRequestID, bIsLast):
+#         pass
+#
+#     # 订阅询价应答
+#     def OnRspSubForQuoteRsp(self, pSpecificInstrument, pRspInfo, nRequestID, bIsLast):
+#         pass
+#
+#     # 取消订阅询价应答
+#     def OnRspUnSubForQuoteRsp(self, pSpecificInstrument, pRspInfo, nRequestID, bIsLast):
+#         pass
+#
+#     # 深度行情通知
+#     def OnRtnDepthMarketData(self, pDepthMarketData):
+#         pass
+#
+#     # 询价通知
+#     def OnRtnForQuoteRsp(self, pForQuoteRsp):
+#         pass
 
 
 class MdApiPy(MdApiWrapper):
     def __init__(self, pszFlowPath="", bIsUsingUdp=False, bIsMulticast=False):
         super(MdApiPy, self).__init__(pszFlowPath, bIsUsingUdp, bIsMulticast)
+
+    def Init(self):
+        super(MdApiPy).Init()
+
+    def Join(self) -> int:
+        return super(MdApiPy).Join()
 
     def ReqUserLogin(self, pReqUserLoginField, nRequestID):
         """
@@ -100,7 +106,7 @@ class MdApiPy(MdApiWrapper):
         """
         return super(MdApiPy, self).GetTradingDay()
 
-    def RegisterFront(self, pszFrontAddress):
+    def RegisterFront(self, pszFrontAddress: str):
         """
         注册前置机网络地址
         @param pszFrontAddress：前置机网络地址。
@@ -108,9 +114,9 @@ class MdApiPy(MdApiWrapper):
         @remark “tcp”代表传输协议，“127.0.0.1”代表服务器地址。”17001”代表服务器端口号。
         :return:
         """
-        return super(MdApiPy, self).RegisterFront(pszFrontAddress)
+        super(MdApiPy, self).RegisterFront(pszFrontAddress.encode())
 
-    def RegisterNameServer(self, pszNsAddress):
+    def RegisterNameServer(self, pszNsAddress: str):
         """
         注册名字服务器网络地址
         @param pszNsAddress：名字服务器网络地址。
@@ -119,7 +125,7 @@ class MdApiPy(MdApiWrapper):
         @remark RegisterNameServer优先于RegisterFront
         :return:
         """
-        return super(MdApiPy, self).RegisterNameServer(pszNsAddress)
+        super(MdApiPy, self).RegisterNameServer(pszNsAddress.encode())
 
     def RegisterFensUserInfo(self, pFensUserInfo):
         """
@@ -127,9 +133,9 @@ class MdApiPy(MdApiWrapper):
         @param pFensUserInfo：用户信息。
         :return:
         """
-        return super(MdApiPy, self).RegisterFensUserInfo(pFensUserInfo)
+        super(MdApiPy, self).RegisterFensUserInfo(pFensUserInfo)
 
-    def SubscribeMarketData(self, pInstrumentID):
+    def SubscribeMarketData(self, pInstrumentID: list):
         """
          订阅行情。
         @param ppInstrumentID 合约ID
@@ -137,7 +143,7 @@ class MdApiPy(MdApiWrapper):
         """
         return super(MdApiPy, self).SubscribeMarketData(pInstrumentID)
 
-    def UnSubscribeMarketData(self, pInstrumentID):
+    def UnSubscribeMarketData(self, pInstrumentID: list):
         """
         退订行情。
         @param ppInstrumentID 合约ID
@@ -145,7 +151,7 @@ class MdApiPy(MdApiWrapper):
         """
         return super(MdApiPy, self).UnSubscribeMarketData(pInstrumentID)
 
-    def SubscribeForQuoteRsp(self, pInstrumentID):
+    def SubscribeForQuoteRsp(self, pInstrumentID: list):
         """
         订阅询价。
         :param pInstrumentID: 合约ID list
@@ -153,10 +159,91 @@ class MdApiPy(MdApiWrapper):
         """
         return super(MdApiPy, self).SubscribeForQuoteRsp(pInstrumentID)
 
-    def UnSubscribeForQuoteRsp(self, pInstrumentID):
+    def UnSubscribeForQuoteRsp(self, pInstrumentID: list):
         """
         退订询价。
         :param pInstrumentID: 合约ID list
         :return:
         """
         return super(MdApiPy, self).UnSubscribeForQuoteRsp(pInstrumentID)
+
+    # for receive message
+
+    # 当客户端与交易后台建立起通信连接时（还未登录前），该方法被调用。
+
+    def OnFrontConnected(self):
+        pass
+
+        # 当客户端与交易后台通信连接断开时，该方法被调用。当发生这个情况后，API会自动重新连接，客户端可不做处理。
+        # @param nReason 错误原因
+        #         0x1001 网络读失败
+        #         0x1002 网络写失败
+        #         0x2001 接收心跳超时
+        #         0x2002 发送心跳失败
+        #         0x2003 收到错误报文
+
+        # 4097 0x1001 网络读失败
+        # 4098 0x1002 网络写失败
+        # 8193 0x2001 读心跳超时
+        # 8194 0x2002 发送心跳超时
+        # 8195 0x2003 收到不能识别的错误消息
+        # 客户端与服务端的连接断开有两种情况：
+        #  网络原因导致连接断开
+        #  服务端主动断开连接
+        # 服务器主动断开连接有两种可能：
+        #  客户端长时间没有从服务端接收报文，时间超时
+        #  客户端建立的连接数超过限制
+
+    def OnFrontDisconnected(self, nReason):
+        pass
+
+        # 心跳超时警告。当长时间未收到报文时，该方法被调用。
+        # @param nTimeLapse 距离上次接收报文的时间
+
+    def OnHeartBeatWarning(self, nTimeLapse):
+        pass
+
+        # 登录请求响应
+
+    def OnRspUserLogin(self, pRspUserLogin, pRspInfo, nRequestID, bIsLast):
+        pass
+
+        # 登出请求响应
+
+    def OnRspUserLogout(self, pUserLogout, pRspInfo, nRequestID, bIsLast):
+        pass
+
+        # 错误应答
+
+    def OnRspError(self, pRspInfo, nRequestID, bIsLast):
+        pass
+
+        # 订阅行情应答
+
+    def OnRspSubMarketData(self, pSpecificInstrument, pRspInfo, nRequestID, bIsLast):
+        pass
+
+        # 取消订阅行情应答
+
+    def OnRspUnSubMarketData(self, pSpecificInstrument, pRspInfo, nRequestID, bIsLast):
+        pass
+
+        # 订阅询价应答
+
+    def OnRspSubForQuoteRsp(self, pSpecificInstrument, pRspInfo, nRequestID, bIsLast):
+        pass
+
+        # 取消订阅询价应答
+
+    def OnRspUnSubForQuoteRsp(self, pSpecificInstrument, pRspInfo, nRequestID, bIsLast):
+        pass
+
+        # 深度行情通知
+
+    def OnRtnDepthMarketData(self, pDepthMarketData):
+        pass
+
+        # 询价通知
+
+    def OnRtnForQuoteRsp(self, pForQuoteRsp):
+        pass
---
+++ examples/md_main.py
@@ -0,0 +1,159 @@
+# encoding:utf-8
+import socket
+import sys
+import urllib.parse
+from contextlib import closing
+import time
+
+from ctpwrapper import ApiStructure
+from ctpwrapper import MdApiPy
+
+
+
+def check_address_port(tcp):
+    """
+    :param tcp:
+    :return:
+    """
+    host_schema = urllib.parse.urlparse(tcp)
+
+    ip = host_schema.hostname
+    port = host_schema.port
+
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
+        if sock.connect_ex((ip, port)) == 0:
+            return True  # OPEN
+        else:
+            return False  # closed
+
+
+class Md(MdApiPy):
+    """
+    """
+
+    def __init__(self, broker_id, investor_id, password, request_id=1):
+        super(Md).__init__()
+
+        self.broker_id = broker_id
+        self.investor_id = investor_id
+        self.password = password
+        self.request_id = request_id
+
+    # def incr_id(self):
+    #
+    #     self.request_id += 1
+
+    def OnRspError(self, pRspInfo, nRequestID, bIsLast):
+
+        self.ErrorRspInfo(pRspInfo, nRequestID)
+
+    def ErrorRspInfo(self, info, request_id):
+        """
+        :param info:
+        :return:
+        """
+        if info.ErrorID != 0:
+            print('request_id=%s ErrorID=%d, ErrorMsg=%s',
+                  request_id, info.ErrorID, info.ErrorMsg.decode('gbk'))
+        return info.ErrorID != 0
+
+    def OnFrontConnected(self):
+        """
+        :return:
+        """
+
+        user_login = ApiStructure.ReqUserLoginField(BrokerID=self.broker_id,
+                                                    UserID=self.investor_id,
+                                                    Password=self.password)
+        self.ReqUserLogin(user_login, self.request_id)
+        print("OnFrontConnected")
+
+    def OnFrontDisconnected(self, nReason):
+
+        print("Md OnFrontDisconnected %s", nReason)
+        sys.exit()
+
+    def OnHeartBeatWarning(self, nTimeLapse):
+        """心跳超时警告。当长时间未收到报文时，该方法被调用。
+        @param nTimeLapse 距离上次接收报文的时间
+        """
+        print('Md OnHeartBeatWarning, time = %s', nTimeLapse)
+
+    def OnRspUserLogin(self, pRspUserLogin, pRspInfo, nRequestID, bIsLast):
+        """
+        用户登录应答
+        :param pRspUserLogin:
+        :param pRspInfo:
+        :param nRequestID:
+        :param bIsLast:
+        :return:
+        """
+        if pRspInfo.ErrorID != 0:
+            print("Md OnRspUserLogin failed error_id=%s msg:%s",
+                  pRspInfo.ErrorID, pRspInfo.ErrorMsg.decode('gbk'))
+        else:
+            print("Md user login successfully")
+            print(pRspUserLogin)
+            print(pRspInfo)
+
+    def OnRtnDepthMarketData(self, pDepthMarketData):
+        """
+        行情订阅推送信息
+        :param pDepthMarketData:
+        :return:
+        """
+        market_data = pDepthMarketData.to_dict()
+        print(market_data)
+
+    def OnRspSubMarketData(self, pSpecificInstrument, pRspInfo, nRequestID, bIsLast):
+        """
+        订阅行情应答
+        :param pSpecificInstrument:
+        :param pRspInfo:
+        :param nRequestID:
+        :param bIsLast:
+        :return:
+        """
+        print("OnRspSubMarketData")
+        print(pSpecificInstrument)
+        print(pRspInfo)
+
+    def OnRspUnSubMarketData(self, pSpecificInstrument, pRspInfo, nRequestID, bIsLast):
+        """
+        取消订阅行情应答
+        :param pSpecificInstrument:
+        :param pRspInfo:
+        :param nRequestID:
+        :param bIsLast:
+        :return:
+        """
+        print("OnRspUnSubMarketData")
+        print(pSpecificInstrument)
+        print(pRspInfo)
+
+
+def main():
+    investor_id = "089303"
+    broker_id = "9999"
+    password = "198759"
+    server = "tcp://180.168.146.187:10011"
+
+    if check_address_port(server):
+
+        md = Md(broker_id,investor_id,password)
+        md.RegisterFront(server)
+        md.Init()
+
+        time.sleep(10)
+        md.SubscribeMarketData(["rb1810"])
+
+
+        print("md_start")
+    else:
+        print("md server down")
+
+
+
+if __name__ == "__main__":
+
+    main()
