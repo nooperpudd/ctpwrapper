a3148219ccaf465866008c4c9217ff290010d3fd
noop f <365504029@qq.com>
2019-05-28 21:25:21 +0800

    add

--- ctp/header/ThostFtdcMdApi.h
+++ ctp/header/ThostFtdcMdApi.h
@@ -107,13 +107,13 @@ public:
 
     ///注册前置机网络地址
     ///@param pszFrontAddress：前置机网络地址。
-    ///@remark 网络地址的格式为：“protocol://ipaddress:port”，如：”tcp://127.0.0.1:17001”。 
+    ///@remark 网络地址的格式为：“protocol://ipaddress:port”，如：”tcp://127.0.0.1:17001”。
     ///@remark “tcp”代表传输协议，“127.0.0.1”代表服务器地址。”17001”代表服务器端口号。
     virtual void RegisterFront(char *pszFrontAddress) = 0;
 
     ///注册名字服务器网络地址
     ///@param pszNsAddress：名字服务器网络地址。
-    ///@remark 网络地址的格式为：“protocol://ipaddress:port”，如：”tcp://127.0.0.1:12001”。 
+    ///@remark 网络地址的格式为：“protocol://ipaddress:port”，如：”tcp://127.0.0.1:12001”。
     ///@remark “tcp”代表传输协议，“127.0.0.1”代表服务器地址。”12001”代表服务器端口号。
     ///@remark RegisterNameServer优先于RegisterFront
     virtual void RegisterNameServer(char *pszNsAddress) = 0;
@@ -127,27 +127,27 @@ public:
     virtual void RegisterSpi(CThostFtdcMdSpi *pSpi) = 0;
 
     ///订阅行情。
-    ///@param ppInstrumentID 合约ID  
+    ///@param ppInstrumentID 合约ID
     ///@param nCount 要订阅/退订行情的合约个数
-    ///@remark 
+    ///@remark
     virtual int SubscribeMarketData(char *ppInstrumentID[], int nCount) = 0;
 
     ///退订行情。
-    ///@param ppInstrumentID 合约ID  
+    ///@param ppInstrumentID 合约ID
     ///@param nCount 要订阅/退订行情的合约个数
-    ///@remark 
+    ///@remark
     virtual int UnSubscribeMarketData(char *ppInstrumentID[], int nCount) = 0;
 
     ///订阅询价。
-    ///@param ppInstrumentID 合约ID  
+    ///@param ppInstrumentID 合约ID
     ///@param nCount 要订阅/退订行情的合约个数
-    ///@remark 
+    ///@remark
     virtual int SubscribeForQuoteRsp(char *ppInstrumentID[], int nCount) = 0;
 
     ///退订询价。
-    ///@param ppInstrumentID 合约ID  
+    ///@param ppInstrumentID 合约ID
     ///@param nCount 要订阅/退订行情的合约个数
-    ///@remark 
+    ///@remark
     virtual int UnSubscribeForQuoteRsp(char *ppInstrumentID[], int nCount) = 0;
 
     ///用户登录请求
