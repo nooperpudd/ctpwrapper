cc042ea2dd6491551265296ed1189c1f131aef18
winton <365504029@qq.com>
2020-10-25 22:06:36 +0800

    drop python3.5 support

--- setup.py
+++ setup.py
@@ -110,7 +110,7 @@ setup(
     platforms=["win32", "linux"],
     packages=["ctpwrapper"],
     package_data={"": package_data},
-    python_requires=">=3.5",
+    python_requires=">=3.6",
     # cython: binding=True
     # binding = true for inspect get callargs
     ext_modules=cythonize(ext_modules,
@@ -129,7 +129,6 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
