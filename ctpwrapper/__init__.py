62fb681988abe29714c4a59388d80d8065886125
Winton <365504029@qq.com>
2021-09-15 21:28:27 +0800

    Update __init__.py
    update version

--- ctpwrapper/__init__.py
+++ ctpwrapper/__init__.py
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with ctpwrapper.  If not, see <http://www.gnu.org/licenses/>.
 
 """
-__version__ = "6.6.1.2"
+__version__ = "6.6.1.3"
 __date__ = "2021-04-06"
 
 from ctpwrapper.Md import MdApiPy
