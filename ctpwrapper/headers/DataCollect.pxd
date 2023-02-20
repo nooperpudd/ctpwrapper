11d0284c5ba157c4e67bbae75c143edddfd68f84
winton <365504029@qq.com>
2023-02-21 00:24:38 +0800

    data collect

--- ctpwrapper/headers/DataCollect.pxd
+++ ctpwrapper/headers/DataCollect.pxd
@@ -1,5 +1,8 @@
 # encoding:utf-8
 # distutils: language=c++
 
+from libc.string cimport const_char
+
 cdef extern from "DataCollect.h":
     int CTP_GetSystemInfo(char *pSystemInfo, int &nLen) nogil except +
+    const_char *CTP_GetDataCollectApiVersion(void) nogil except +
---
+++ tests/test_datacollect.py
@@ -0,0 +1,28 @@
+# encoding:utf-8
+"""
+(Copyright) 2018, Winton Wang <365504029@qq.com>
+
+ctpwrapper is free software: you can redistribute it and/or modify
+it under the terms of the GNU LGPLv3 as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ctpwrapper.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+import unittest
+
+
+from ctpwrapper import datacollect
+
+
+class DataCollectTest(unittest.TestCase):
+    def test_data_collect_api(self):
+        def test_get_collect_version(self):
+            print(datacollect.GetDataCollectApiVersion())
